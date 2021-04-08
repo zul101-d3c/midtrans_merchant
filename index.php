fe7f3c10bfc5581a23809f32ee29c4a4baa1d4c9
zul101 <zulkifli.d3c@gmail.com>
2021-04-08 11:27:04 +0700

    index.php

--- index.php
+++ index.php
@@ -10,10 +10,14 @@ $api_url = $is_production ?
 
 
 // Check if request doesn't contains `/charge` in the url/path, display 404
+/*
 if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
   http_response_code(404); 
   echo "wrong path, make sure it's `/charge`"; exit();
 }
+*/
+echo strpos($_SERVER['REQUEST_URI'];
+
 // Check if method is not HTTP POST, display 404
 if( $_SERVER['REQUEST_METHOD'] !== 'POST'){
   http_response_code(404);
